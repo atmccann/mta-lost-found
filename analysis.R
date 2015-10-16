726d2227d7c396ce599eacfaf0bc48bcd73d94a1
atmccann <allisontmccann@gmail.com>
2015-10-16 17:12:45 -0400

    update r script

--- analysis.R
+++ analysis.R
@@ -3,27 +3,35 @@ require(scales)
 require(ggplot2)
 library(data.table)
 require(dplyr)
+require(tidyr)
 library(jsonlite)
 library(lubridate)
 
 setwd("~/Documents/FiveThirtyEight/interactives/mta-lost-found/data")
 
 fileNames <- Sys.glob("*.json")
-head(fileNames)
-
-#get dates
-date <- gsub(".json", "", fileNames)
-View(date)
+View(fileNames)
 
 allData <- list()
 for (fileName in fileNames) {
   data <- as.data.frame(fromJSON(fileName, flatten = TRUE))
-  data$Date <- gsub(".json", "", fileName)
+  data$date <- gsub(".json", "", fileName)
   allData[[fileName]] <- data
 }
 
 #combine all data 
 combineData <- rbind.fill(allData)
 data <- combineData
-View(data)
+
+data$date <- parse_date_time(data$date, "%Y-%m-%d")
+head(data)
+
+newData <- gather(data, items, amount, -date)
+View(newData)
+
+plot <- ggplot(newData, aes(x = date, y = amount, group = items, col = items)) +
+  geom_line()
+
+plot
+
 
