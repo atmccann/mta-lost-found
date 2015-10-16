cc3e3e7a2faecc6ea64900c981a8bcc26220bfd5
atmccann <allisontmccann@gmail.com>
2015-10-16 16:30:13 -0400

    remove old code

--- analysis.R
+++ analysis.R
@@ -27,17 +27,3 @@ combineData <- rbind.fill(allData)
 data <- combineData
 View(data)
 
-#ONE BY ONE
-aug17 <- as.data.frame(fromJSON("2014-08-17.json", flatten = TRUE))
-aug17$Date <- c("2014-08-17")
-View(aug17)
-head(aug17)
-
-aug18 <- as.data.frame(fromJSON("2014-08-18.json", flatten = TRUE))
-View(aug18)
-
-combined <-rbind.fill(aug17, aug18)
-View(combined)
-head(combined)
-
-
